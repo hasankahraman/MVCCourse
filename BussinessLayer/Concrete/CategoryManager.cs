46d28b2a2581f3007f4bc4e432113a73ae94d2fa
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2022-11-29 00:53:05 +0300

    bussiness layer'a geçildi

---
+++ BussinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repository.List();
+        }
+
+        public void CategoryAddBL(Category c)
+        {
+            if (c.Name.Length<=3 || c.Name==""|| c.Description=="" || c.Name.Length>=51)
+            {
+                //hata mesajı throw
+            }
+            else
+            {
+                repository.Insert(c);
+            }
+        }
+
+
+
+    }
+}
