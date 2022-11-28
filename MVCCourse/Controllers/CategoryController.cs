8c17a998ceec86760338a1daf95151af1036abbc
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2022-11-29 01:22:20 +0300

    end of course 23

--- MVCCourse/Controllers/CategoryController.cs
+++ MVCCourse/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BussinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace MVCCourse.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager();
+
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categories = cm.GetAllBL();
+            return View(categories);
         }
     }
 }
