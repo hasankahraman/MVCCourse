4ae066a82a248645309132777082abc3d6aa4ce3
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2022-12-05 15:20:39 +0300

    course 60 continuie

---
+++ BussinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.ValidationRules
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.UserMail).NotEmpty().WithMessage("E-Posta alanı boş olamaz.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu alanı boş olamaz.");
+            RuleFor(x => x.Message).NotEmpty().WithMessage("Mesaj alanı boş olamaz.");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı alanı boş olamaz.");
+            RuleFor(x => x.UserName).MinimumLength(3).WithMessage("Kullanıcı adı en az 3 karakter olmalıdır.");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Konu adı en az 3 karakter olmalıdır.");
+            RuleFor(x => x.Subject).MaximumLength(250).WithMessage("Konu adı en fazla 250 karakter olmalıdır.");
+        }
+    }
+}
--- MVCCourse/Controllers/ContactController.cs
+++ MVCCourse/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BussinessLayer.Concrete;
+using BussinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,18 @@ namespace MVCCourse.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        ContactManager contactManager = new ContactManager(new EFContactDAL());
+        ContactValidator validator = new ContactValidator();
         public ActionResult Index()
         {
-            return View();
+            var contacts = contactManager.GetList();
+            return View(contacts);
+        }
+        //[HttpGet]
+        public ActionResult GetContactMessageDetails(int id)
+        {
+            var contact = contactManager.GetById(id);
+            return View(contact);
         }
     }
 }
